adfceb160e23dcf6584b9d1a36a4a02b06b1007c
YordanMilanov <dancho.galinov@gmail.com>
2023-10-11 19:01:14 +0300

    custom model binder implemented

---
+++ HouseRenting/HouseRentingSystem.Web.Infrastructure/ModelBinders/DecimalModelBinder.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace HouseRentingSystem.Web.Infrastructure.ModelBinders
+{
+    internal class DecimalModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext? bindingContext)
+        {
+            if (bindingContext == null)
+            {
+                throw new ArgumentNullException(nameof(bindingContext));
+            }
+
+            ValueProviderResult valueResult =
+                bindingContext.ValueProvider
+                    .GetValue(bindingContext.ModelName);
+            if (valueResult != ValueProviderResult.None
+                && string.IsNullOrWhiteSpace(valueResult.FirstValue))
+            {
+                decimal parsedValue = 0m;
+                bool binderSuccseded = false;
+
+                try
+                {
+                    string formDecValue = valueResult.FirstValue;
+                    formDecValue = formDecValue.Replace(",", CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+                    formDecValue = formDecValue.Replace(".", CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+
+                    parsedValue = Convert.ToDecimal(formDecValue);
+                }
+                catch (Exception e)
+                {
+                    bindingContext.ModelState.AddModelError(bindingContext.ModelName, e, bindingContext.ModelMetadata);
+                }
+
+                if (binderSuccseded)
+                {
+                    bindingContext.Result = ModelBindingResult.Success(parsedValue);
+                }
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ HouseRenting/HouseRentingSystem.Web.Infrastructure/ModelBinders/DecimalModelBinderProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace HouseRentingSystem.Web.Infrastructure.ModelBinders
+{
+    public class DecimalModelBinderProvider : IModelBinderProvider
+    {
+        public IModelBinder GetBinder(ModelBinderProviderContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (context.Metadata.ModelType == typeof(decimal) || 
+                context.Metadata.ModelType == typeof(decimal?))
+            {
+                return new DecimalModelBinder();
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
